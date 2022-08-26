3976f731d75c759bee2f80f04c94c8361ebfb414
Ethan Nitz <etnitz@gmail.com>
2022-08-26 12:10:54 -0700

    added empId

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ from tkinter import *
 window = Tk()
 window.geometry('600x270')
 window.title('Employee Database App')
+
+empId = Label(window, text='Employee ID', font=('Serif', 12))
+empId.place(x=20, y=30)
+
 window.mainloop()
