7e97acb58be0b756866f4d5c8dc842808bf43a84
Ethan Nitz <etnitz@gmail.com>
2022-08-30 11:56:18 -0700

    started enterData func

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import mysql.connector
 
 window = Tk()
 window.geometry('650x270')
@@ -22,22 +23,28 @@ enterName.place(x=230, y=60)
 enterDept = Entry(window)
 enterDept.place(x=230, y=90)
 
-insertBtn = Button(window, text='Insert', font=('Serif', 12), bg='white')
+insertBtn = Button(window, text='Insert', font=('Serif', 12), bg='white', command=insertData)
 insertBtn.place(x=20, y=160)
 
-updateBtn = Button(window, text='Update', font=('Serif', 12), bg='white')
+updateBtn = Button(window, text='Update', font=('Serif', 12), bg='white', command=updateData)
 updateBtn.place(x=100, y=160)
 
-getBtn = Button(window, text='Retrieve', font=('Serif', 12), bg='white')
+getBtn = Button(window, text='Retrieve', font=('Serif', 12), bg='white', command=getData)
 getBtn.place(x=190, y=160)
 
-deleteBtn = Button(window, text='Delete', font=('Serif', 12), bg='red')
+deleteBtn = Button(window, text='Delete', font=('Serif', 12), bg='red', command=deleteData)
 deleteBtn.place(x=290, y=160)
 
-resetBtn = Button(window, text='Reset', font=('Serif', 12), bg='yellow')
+resetBtn = Button(window, text='Reset', font=('Serif', 12), bg='yellow', command=resetFields)
 resetBtn.place(x=20, y=200)
 
 showData = Listbox(window)
 showData.place(x=450, y=30)
 
 window.mainloop()
+
+def insertData():
+    id = enterId.get()
+    name = enterName.get()
+    dept = enterDept.get()
+    
