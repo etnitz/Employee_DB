44d81060219e91fc7a3756c962c282ac271862c7
Ethan Nitz <etnitz@gmail.com>
2022-09-13 14:13:41 -0700

    created get and delete funcs

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from email import message
 from tkinter import *
 from tkinter import messagebox
 import mysql.connector
@@ -47,6 +48,39 @@ def updateData():
         messagebox.showinfo('Update Status','Data updated sucessfully!')
         myDB.close()
 
+def getData():
+    id = enterId.get()
+    if id == '':
+        messagebox.showwarning('Fetch Status', "Please provide the employee ID to fetch the data")
+    else:
+        myDB = mysql.connector.connect(host='localhost', user='root', password='password', database='employee')
+        myCur = myDB.cursor()
+        getQuery = "SELECT * FROM staff WHERE staff_id = %s"
+        record = (id)
+        myCur.execute(getQuery, record)
+        rows = myCur.fetchall()
+        for row in rows:
+            enterName.insert(0, row[1])
+            enterDept.insert(0, row[2])
+        myDB.close()
+
+def deleteData():
+    id = enterId.get()
+    if id == '':
+        messagebox.showwarning('Cannot Delete', 'Please provide employee ID to delete the data')
+    else:
+        myDB = mysql.connector.connect(host='localhost', user='root', password='password', database='employee')
+        myCur = myDB.cursor()
+        myCur.execute("DELETE FROM staff WHERE staff_id = 'enterId.get()'")
+        myDB.commit()
+
+        enterId.delete(0, 'end')
+        enterName.delete(0, 'end')
+        enterDept.delete(0, 'end')
+
+        messagebox.showinfo("Delete Status", 'Data deleted successfully')
+        myDB.close()
+
 window = Tk()
 window.geometry('650x270')
 window.title('Employee Database App')
@@ -75,10 +109,10 @@ insertBtn.place(x=20, y=160)
 updateBtn = Button(window, text='Update', font=('Serif', 12), bg='white', command=updateData)
 updateBtn.place(x=100, y=160)
 
-getBtn = Button(window, text='Retrieve', font=('Serif', 12), bg='white')
+getBtn = Button(window, text='Retrieve', font=('Serif', 12), bg='white', command=getData)
 getBtn.place(x=190, y=160)
 
-deleteBtn = Button(window, text='Delete', font=('Serif', 12), bg='red')
+deleteBtn = Button(window, text='Delete', font=('Serif', 12), bg='red', command=deleteData)
 deleteBtn.place(x=290, y=160)
 
 resetBtn = Button(window, text='Reset', font=('Serif', 12), bg='yellow')
